c0db0fcea86af9b9ab420c1de3fbab3ac964510a
gwangmin <rhkdrhkd2222@naver.com>
2025-08-03 22:35:20 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,12 @@
+coins = [25,10,5,1]
+n = int(input())
+
+for _ in range(n):
+    c = int(input())
+    result = []
+
+    for coin in coins:
+        result.append(c // coin)
+        c %= coin #c = c % coin
+
+    print(*result)
